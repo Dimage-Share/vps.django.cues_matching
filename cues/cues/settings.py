b7261edfc85b4d0414611e7e38af56487aaa2ae1
nebuka.keita <keita-nebuka@dimage.co.jp>
2025-11-01 15:13:14 +0900

    database profile added into settings.py

--- cues/cues/settings.py
+++ cues/cues/settings.py
@@ -71,9 +71,13 @@ WSGI_APPLICATION = "cues.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cues_db',
+        'USER': 'app',
+        'PASSWORD': '!~G2g%Y+.P+6',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
